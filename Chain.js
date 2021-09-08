36a5dde68603d6da6b59327fbc8285331e418d30
Max069 <misstatudi@gmail.com>
2021-09-08 06:24:35 -0700

    Tram

--- Chain.js
+++ Chain.js
@@ -1,18 +1,18 @@
-class SlingShot{
+class Chain{
     constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            pointB: pointB,
+            bodyB: pointB,
             stiffness: 0.5            
         }
-        this.sling1 = loadImage('sprites/sling1.png');
+        //this.sling1 = loadImage('sprites/sling1.png');
        
         this.sling = Constraint.create(options);
-        World.add(world, this.sling);
+        World.add(myWorld, this.sling);
     }
    
    show(){
-        image(this.sling1,200,20);
+        //image(this.sling1,200,20);
         
      
             var pointA = this.sling.bodyA.position;
--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var myEngine, myWorld;
 var bg;
 var ground;
 var boggie1,boggie2,boggie3;
+var stone
 var chain1;
 var trainSound 
 var crashSound
@@ -24,6 +25,21 @@ function setup() {
 
   ground = new Ground(600,380,1200,20);
 
+  boggie1 = new Bogie(50,100,60,60);
+  boggie2 = new Bogie(150,100,60,60);
+  boggie3 = new Bogie(250,100,60,60);
+  boggie4 = new Bogie(350,100,60,60);
+  boggie5 = new Bogie(450,100,60,60);
+  boggie6 = new Bogie(550,100,60,60);
+
+  stone = new Stone(1100,100,150,150);
+
+  chain1 = new Chain(boggie1.body,boggie2.body);
+  chain2 = new Chain(boggie2.body,boggie3.body);
+  chain3 = new Chain(boggie3.body,boggie4.body);
+  chain4 = new Chain(boggie4.body,boggie5.body);
+  chain5 = new Chain(boggie5.body,boggie6.body);
+
 
 }
 
@@ -31,4 +47,40 @@ function draw() {
   background(bg);  
   Engine.update(myEngine);
 
+var collision = Matter.SAT.collides(boggie6.body,stone.body)
+
+if(collision.collided){
+  flag=1
+}
+
+if(flag ===1){
+  textSize(40)
+  fill("red")
+  text ("Crashed",600,200)
+  crashSound.play()
+
+
+}
+
+  boggie1.show()
+  boggie2.show()
+  boggie3.show()
+  boggie4.show()
+  boggie5.show()
+  boggie6.show()
+
+  stone.show()
+
+  chain1.show()
+  chain2.show()
+  chain3.show()
+  chain4.show()
+  chain5.show()
+
+  }
+
+  function keyPressed(){
+  if(keyCode === RIGHT_ARROW){
+    Matter.Body.applyForce(boggie6.body, boggie6.body.position, {x:0.5, y:0})
+  }
   }
